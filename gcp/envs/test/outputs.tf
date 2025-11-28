b167fa9877ab69a47c5909f9daad05e1b6cdefcf
logic <logic@Logicinhk.local>
2025-11-28 13:21:57 +0800

    feature: add output

--- gcp/envs/test/outputs.tf
+++ gcp/envs/test/outputs.tf
@@ -1,49 +1,49 @@
 output "network_name" {
   description = "VPC network name"
-  value       = module.network.network_name
+  value       = module.gcp.network_name
 }
 
 output "subnets" {
-  description = "List of subnets created by the network module"
-  value       = module.network.subnets
+  description = "List of subnets"
+  value       = module.gcp.subnets
 }
 
 output "nat_router_names" {
   description = "NAT router names"
-  value       = module.nat.router_names
+  value       = module.gcp.nat_router_names
 }
 
 output "nat_names" {
   description = "NAT configuration names"
-  value       = module.nat.nat_names
+  value       = module.gcp.nat_names
 }
 
 output "service_accounts" {
-  description = "Service accounts created by the IAM module"
-  value       = module.iam.service_accounts
+  description = "Service accounts"
+  value       = module.gcp.service_accounts
 }
 
 output "gcs_bucket_names" {
   description = "GCS bucket names"
-  value       = module.gcs.bucket_names
+  value       = module.gcp.gcs_bucket_names
 }
 
 output "gcs_bucket_urls" {
   description = "GCS bucket URLs"
-  value       = module.gcs.bucket_urls
+  value       = module.gcp.gcs_bucket_urls
 }
 
 output "vm_instance_names" {
   description = "VM instance names"
-  value       = module.gce.instance_names
+  value       = module.gcp.vm_instance_names
 }
 
 output "vm_instance_ips" {
   description = "VM instance external IPs"
-  value       = module.gce.instance_ips
+  value       = module.gcp.vm_instance_ips
 }
 
 output "instance_group_names" {
   description = "Instance group names"
-  value       = module.gce.instance_group_names
+  value       = module.gcp.instance_group_names
 }
